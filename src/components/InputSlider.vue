69deb33b018fe70b3c944b2574d90fd734ffdc0e
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-05-23 16:32:23 -0600

    elementos pal form demas vistas v5

---
+++ src/components/InputSlider.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    
+    data() {
+        return {
+            porcentaje: 50
+        }
+    },
+    props:{index:null},
+    methods: {
+        PasarDato() {
+            this.$emit('porcentaje', this.porcentaje, this.index)
+        }
+    }
+}
+</script>
+
+<template>
+    <input type="range" v-model="porcentaje" class="w-full py-3 rounded-lg transition-colors bg-gray-100 focus:shadow focus:bg-white focus:outline-none" id="porcentaje" placeholder="" typeof="slider" min="0" max="100" @change="PasarDato"/>
+</template>
