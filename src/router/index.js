e4ff417e3bffd7204ef0d61e2aebd05f2983917a
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-05-23 16:20:18 -0600

    creacion ruta registros

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,16 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
+    {
+      path: "/RegistroPersonas",
+      name: "Registro de Personas",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/PersonasView.vue"),
+    },
+
+
   ],
 });
 
