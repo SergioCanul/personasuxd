957896b3b1fb09e2d9ffc52eaaeb29f143a1d870
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-05-23 16:18:25 -0600

    agracion de api y nuevo welcome

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template class="flex-col justify-center bg-blue-100 w-full h-full p-2">
+
+  <div class="w-full h-full bg-gradient-to-r from-slate-500 to-gray-400 justify-items-center">
+      
+      <header>
+        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="/"  class="flex items-center">
+            <img src="../src/assets/logopersona.svg" class="h-8 mr-3" alt="logo" />
+            <span class="self-center text-2xl font-bold whitespace-nowrap dark:text-pink-700">PERSONAS UX</span>
+        </a>
+    </div>
+</nav>
+<nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto">
+        <div class="flex items-center">
+            <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                <li>
+                    <RouterLink class="text-gray-900 dark:text-white hover:text-amber-400 hover:underline" to="/">Inicio</RouterLink>
+                </li>
+                <li>
+                    <RouterLink class="text-gray-900 dark:text-white hover:text-pink-600 hover:underline" to="/RegistroPersonas">Nuevo registro</RouterLink>
+                </li>
+                <li>
+                  <RouterLink class="text-gray-900 dark:text-white hover:text-cyan-500 hover:underline" to="/ListaPersonas">Personas registradas</RouterLink>
+                </li>
+                <li>
+                  <RouterLink class="text-gray-900 dark:text-white hover:text-emerald-300 hover:underline" to="/BuscadorPersonas">Buscador de personas</RouterLink>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>  
+</header>
+  
+  
+    <RouterView />
+  </div>
+  </template>
---
+++ src/components/Bienvenida.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template class="w-full">
+     <div class="px-6 py-12 md:px-12 h-screen bg-transparent text-gray-900 text-center lg:text-left">
+      <div class="container mx-auto xl:px-32">
+        <div class="grid lg:grid-cols-2 gap-12 flex items-center">
+          <div class="mt-12 lg:mt-0">
+            <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Añade perfiles <br /><span class="text-pink-700">de personas </span></h1>
+            <a class="inline-block px-7 py-3 mr-2 bg-pink-700 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-pink-900 hover:shadow-lg focus:bg-pink-950 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-fuchsia-900 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="/RegistroPersonas" role="button">Añadir persona</a>
+            <a class="inline-block px-7 py-3 bg-transparent text-blue-950 font-bold text-sm leading-snug uppercase rounded hover:text-pink-800 hover:underline hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="/ListaPersonas" role="button">Ver registros</a>
+          </div>
+          <div class="mb-12 lg:mb-0">
+            <img
+              src="../components/icons/personasux.png"
+              class="w-full rounded-lg object-contain"
+              alt="personas"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+</template>
