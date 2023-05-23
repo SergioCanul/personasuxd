64b684ff03fbda46e14ba01b32629ff58223ef09
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-05-23 16:30:05 -0600

    elementos pal form demas vistas v2

---
+++ src/components/InputText.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+    
+    data() {
+        return {
+            datoIT: '',
+        }
+    },
+    methods: {
+        //Funcion para hacer un emit
+        PasarDato() {
+            
+            this.$emit('dataIT',this.datoIT)
+        }
+    }
+}
+</script>
+
+<template>
+    <input type="text" v-model="datoIT" name="dataIT" id="dataIT" class="w-full py-2.5 px-4 opacity-70 rounded-md bg-gray-50 focus:shadow focus:bg-white focus:outline-none" @input="PasarDato"/>
+</template>
