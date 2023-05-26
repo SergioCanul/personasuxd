c27282c04510548718ff6b0981970625de2560b0
SergioCanul <sergio.18070014@itsmotul.edu.mx>
2023-05-26 01:06:26 -0600

    Vista buscador

---
+++ src/views/BuscadorPersonasView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BuscarPersonas from "../components/BuscarPersonas.vue";
+</script>
+
+<template>
+  <div class="p-5">
+
+    <BuscarPersonas/>
+
+  </div>
+  
+</template>
+
+<style>
+
+</style>
